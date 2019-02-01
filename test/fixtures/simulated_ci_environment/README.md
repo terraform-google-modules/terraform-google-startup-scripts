54b385cf5df0c8d681cf2d398595cb9644d164ae
Jeff McCune <jmccune@google.com>
2019-01-31 16:58:16 -0800

    Manage GCP fixture resources like CI does
    Without this patch there isn't a clear way to run integration tests
    locally on a workstation reflecting how the CI system runs integration
    tests.  This is a problem because it creates drift and divergence from
    how CI operates, creating friction in the development process.
    
    This patch addresses the problem by allowing GCP resources to be created
    and managed just like they are with the CI system.  A project and
    service account are created and provided as inputs to the integration
    test suite.

---
+++ test/fixtures/simulated_ci_environment/README.md
@@ -0,0 +1,53 @@
+# Integration Testing
+
+Use this directory to create resources reflecting the same resource fixtures
+created for use by the CI environment CI integration test pipelines.  The intent
+of these resources is to run the integration tests locally as closely as
+possible to how they will run in the CI system.
+
+Once created, store the service account key content into the
+`GOOGLE_CREDENTIALS` environment variable.  This reflects the same behavior as
+used in CI.
+
+For example:
+
+```bash
+terraform init
+terraform apply
+terraform output phoogle_sa > ~/.credentials/startup-scripts-sa.json
+```
+
+Then, configure the environment (suggest using direnv) like so:
+
+```bash
+export GOOGLE_CREDENTIALS_PATH="${HOME}/.credentials/startup-scripts-sa.json"
+export GOOGLE_CREDENTIALS="$(<"${GOOGLE_CREDENTIALS_PATH}")"
+export TF_VAR_project_id="startup-scripts"
+export TF_VAR_region="us-west1"
+```
+
+With these variables set, the `make integration_test_run` task should work
+locally and execute similar to how CI executes the integration test job.
+
+For example:
+
+[^]: (autogen_docs_start)
+
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|:----:|:-----:|:-----:|
+| billing_account | The billing account id associated with the project, e.g. XXXXXX-YYYYYY-ZZZZZZ | string | - | yes |
+| folder_id | The numeric folder id to create resources | string | - | yes |
+| organization_id | The numeric organization id | string | - | yes |
+| project_id | The project_id to deploy the example instance into.  (e.g. "simple-sample-project-1234") | string | - | yes |
+| region | The region to deploy to | string | - | yes |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| phoogle_sa | The SA KEY JSON content.  Store in GOOGLE_CREDENTIALS. |
+
+[^]: (autogen_docs_end)
---
+++ test/fixtures/simulated_ci_environment/main.tf
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+provider "google" {
+  project = "${var.project_id}"
+  region  = "${var.region}"
+}
+
+locals {
+  required_service_account_project_roles = [
+    "roles/compute.instanceAdmin",
+    "roles/iam.serviceAccountUser",
+  ]
+}
+
+resource "google_project" "startup_scripts" {
+  name            = "startup-scripts"
+  project_id      = "startup-scripts"
+  folder_id       = "${var.folder_id}"
+  billing_account = "${var.billing_account}"
+}
+
+resource "google_project_services" "startup_scripts" {
+  project = "${google_project.startup_scripts.id}"
+  services = [
+    "cloudresourcemanager.googleapis.com",
+    "storage-api.googleapis.com",
+    "compute.googleapis.com",
+    "oslogin.googleapis.com",
+  ]
+}
+
+resource "google_service_account" "startup_scripts" {
+  project      = "${google_project.startup_scripts.id}"
+  account_id   = "ci-startup-scripts"
+  display_name = "ci-startup-scripts"
+}
+
+resource "google_project_iam_binding" "startup_scripts" {
+  count   = "${length(local.required_service_account_project_roles)}"
+  project = "${google_project_services.startup_scripts.project}"
+  role    = "${element(local.required_service_account_project_roles, count.index)}"
+  members = [
+    "serviceAccount:${google_service_account.startup_scripts.email}",
+  ]
+}
+
+resource "google_service_account_key" "startup_scripts" {
+  service_account_id = "${google_service_account.startup_scripts.id}"
+}
