6ee09216510675a6b60a8296551203cdd756b46b
Jeff McCune <jeff@openinfrastructure.co>
2019-02-19 21:03:11 -0800

    (WIP) Fixup race condition collecting serial console output
    Without this patch the instance startup sequence may not be completed when the
    test suite collects gcloud console output for testing.  This creates a race
    condition.
    
    This patch addresses the problem by retrying the gcloud command until the
    marker `systemd: Startup finished` is found in the output.

--- test/integration/gsutil/controls/gcloud.rb
+++ test/integration/gsutil/controls/gcloud.rb
@@ -12,16 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-project_id = attribute('project_id')
-region = attribute('region')
-zone = "#{region}-a"
+require 'retriable'
 
 control 'get_from_bucket with crcmod compilation' do
   title "With enable_init_gsutil_crcmod_el=true and enable_get_from_bucket=true"
 
   describe 'console output of startup-scripts-gsutil1' do
-    let :subject do
-      command("gcloud compute instances get-serial-port-output startup-scripts-gsutil1 --project #{project_id} --zone #{zone}")
+    # Avoid racing against the instance boot sequence
+    before :all do
+      Retriable.retriable(tries: 20) do
+        get_serial_port_output = "gcloud compute instances get-serial-port-output startup-scripts-gsutil1"
+        @cmd = command("#{get_serial_port_output} --project #{attribute('project_id')} --zone #{attribute('region')}-a")
+        if not %r{systemd: Startup finished}.match(@cmd.stdout)
+          raise StandardError, "Not found: 'systemd: Startup finished' in console output, cannot proceed"
+        end
+      end
+    end
+
+    subject do
+      @cmd
     end
 
     describe "Overall result of startup-script-custom" do
@@ -35,26 +44,26 @@ control 'get_from_bucket with crcmod compilation' do
     end
 
     describe "gsutil version -l before calling stdlib::init_gsutil_crcmod_el" do
-      its('stdout') { should match('679EBF864666 compiled crcmod: False') }
+      its('stdout', focus: true) { should match('679EBF864666 compiled crcmod: False') }
     end
 
     describe "gsutil version -l after calling stdlib::init_gsutil_crcmod_el" do
       its('stdout') { should match('28BBEF21C095 compiled crcmod: True') }
     end
 
-    # context "when the instance does not have storage.objects.get access to the bucket" do
-    #   describe "stdlib::get_from_bucket should retry up to 10 times" do
-    #     9.times do |n|
-    #       its('stdout') { should match(%r{reported non-zero exit code fetching gs://startup-scripts-\w+/message\.txt.*?Retrying \(#{n+1}/10\)}) }
-    #     end
-    #   end
-    # end
-
-    context "stdlib::get_from_bucket -u gs://<bucket>/message.txt -d /path/to/tmpdir" do
-      describe "the content of message.txt fetched using stdlib::get_from_bucket" do
-        its('stdout') { should match('EXPECTED: Hello World! uuid=0afce28a-057b-42cf-a90f-493de3c0666b') }
-        its('stdout') { should match('ACTUAL: Hello World! uuid=0afce28a-057b-42cf-a90f-493de3c0666b') }
+    context "when the instance does not have storage.objects.get access to the bucket" do
+      describe "stdlib::get_from_bucket should retry up to 10 times" do
+        9.times do |n|
+          its('stdout') { should match(%r{reported non-zero exit code fetching gs://startup-scripts-\w+/message\.txt.*?Retrying \(#{n+1}/10\)}) }
+        end
       end
     end
+
+    # context "stdlib::get_from_bucket -u gs://<bucket>/message.txt -d /path/to/tmpdir" do
+    #   describe "the content of message.txt fetched using stdlib::get_from_bucket" do
+    #     its('stdout') { should match('EXPECTED: Hello World! uuid=0afce28a-057b-42cf-a90f-493de3c0666b') }
+    #     its('stdout') { should match('ACTUAL: Hello World! uuid=0afce28a-057b-42cf-a90f-493de3c0666b') }
+    #   end
+    # end
   end
 end
