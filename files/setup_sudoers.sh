2ce06d4cb548cef26b50cfb8ee5395516ede5964
Eric Malloy <malloy.j.eric@gmail.com>
2019-02-25 20:01:02 -0500

    refactor setup_sudoers to add entries from metadata as files in /etc/sudoers.d
    +squash, add license header to setup_sudoers
    
    +squash add todo for new issue to be created
    
    stage sudoers entry to /etc/sudoers.d/\ instead of /etc/sudoers. check if exists in sudoers and in /etc/sudoers.d +WIP
    
    +drop this commit , WIP. This functionality will be replicated into a simple sudoers check/validation
    
    finish cleanup, +sq
    
    remove todo, moved to bug

--- files/setup_sudoers.sh
+++ files/setup_sudoers.sh
@@ -1,40 +1,53 @@
 #! /bin/bash
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Read the project metadata key named "sudoers" and add each comma separated value to
 # the sudoers file.
 stdlib::setup_sudoers() {
   local user user_list sudoers_file
-  user_list="$(metadata_get -k 'project/attributes/sudoers')"
+  user_list="$(stdlib::metadata_get -k 'project/attributes/sudoers')"
 
   if [[ -z "${user_list}" ]]; then
-    stdlib::debug "Skipping sudoers setup.  The value of the project metadata key named sudoers is empty.  Set sudoers to a comma separated list to enable sudo support, e.g. sudoers=jmccune,pames"
+    stdlib::debug "Skipping sudoers setup.  \
+                   The value of the project metadata key named sudoers is empty. \
+                   Set sudoers to a comma separated list to enable sudo \
+                   support, e.g. sudoers=jmccune,pames"
     return 0
   fi
 
-  sudoers_file="$(mktemp)"
-  cp /etc/sudoers "${sudoers_file}"
+  sudoers_file="/etc/sudoers"
+  sudoers_d="/etc/sudoers.d"
 
   for user in ${user_list//,/ }; do
-    if grep -q "^${user}"'\b' "${sudoers_file}"; then
-      stdlib::debug "User ${user} is already in /etc/sudoers, taking no action"
+    if [[ -f  "${sudoers_d}/${user}" ]] \
+      && ( grep -q "^${user}"'\b' "${sudoers_d}/${user}" ) \
+      || (grep -q "^${user}"'\b' "${sudoers_file}")  ; then
+      stdlib::debug "User ${user} is already in /etc/sudoers or \
+                    /etc/sudoers.d/${user}, taking no action"
     else
-      stdlib::info "Adding ${user} to /etc/sudoers"
-      echo -e "${user}\tALL= (ALL)\tNOPASSWD: ALL" >>"${sudoers_file}"
+      stdlib::info "Adding ${user} to /etc/sudoers.d/${user}"
+      echo -e "${user}\tALL= (ALL)\tNOPASSWD: ALL" \
+      > "${sudoers_d}/${user}" \
+      && chmod 0440 "${sudoers_d}/${user}"
     fi
   done
 
-  if cmp --silent /etc/sudoers "${sudoers_file}"; then
-    stdlib::debug "No changes necessary to sudoers file, doing nothing."
-    return 0
+  if visudo -c ; then
+    stdlib::info "sudoers config valid!"
   else
-    stdlib::info "Staged changes to sudoers:"
-    diff -U2 /etc/sudoers "${sudoers_file}" || true
-    if visudo -cf "${sudoers_file}"; then
-      stdlib::info "Installing new /etc/sudoers file"
-      stdlib::cmd install -o 0 -g 0 -m 0440 "${sudoers_file}" /etc/sudoers
-      return $?
-    else
-      stdlib::error "Skipping modification of /etc/sudoers because the temporary sudoers file is invalid."
-      return 1
-    fi
+    stdlib::error "sudoers config invalid!"
+    return 1
   fi
 }
