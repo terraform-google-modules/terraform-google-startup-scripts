e90d8589616b8d872344f2bbc2079f6b925a9862
Awais Malik <awmalik@google.com>
2024-01-02 09:41:22 -0800

    chore: upgraded versions.tf to include minor bumps from tpg v5 (#45)
    Co-authored-by: Andrew Peabody <andrewpeabody@google.com>

--- test/setup/versions.tf
+++ test/setup/versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 2.13.0, < 5.0"
+      version = ">= 2.13.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 2.13.0, < 5.0"
+      version = ">= 2.13.0"
     }
   }
 }
